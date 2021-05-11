956c5871bd3bd74a44442ad8adb5e46208f3fff6
Miguelcrack09 <m.andres09@hotmail.com>
2021-05-10 19:18:12 -0500

    Updated

---
+++ Pruebas.py
@@ -0,0 +1,27 @@
+uno = 19;dos = 25;tres = 62;cuatro=52
+suma = uno+dos+tres+cuatro
+print ("Hola, la suma es",suma)
+promedio = suma / 4
+print ("hola, ya hicimos tu promedio, el cual es:", promedio)
+
+cadena1 = 'comillas simples'
+print (cadena1)
+
+cadena2 = "comillas dobles"
+print (cadena2)
+
+n = "Aprender"
+a = "Python"
+na = n + " " + a
+print (na)
+
+salario = 1000000; veinte = 0.2; quince = 0.15; cinco = 0.05
+porveinte = salario*veinte; porquince = salario*quince; porcinco = salario*cinco
+suma = porveinte+(2*porquince)+porcinco; resta = salario-suma
+print ("la suma de los gastos es:", int(suma), "\n Lo que le queda es:", int(resta))
+
+
+b = ['2.36', 'elefante', 1010, 'rojo']
+print(b)
+catorce = b[0:3:2]
+print(catorce)
